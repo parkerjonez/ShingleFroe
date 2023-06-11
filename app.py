3895f4b4f4467d069671fa83c0a5a3bc46232fdb
Towerhouser <parker@towerhouse.digital>
2023-06-10 22:15:23 -0700

    Going back to a normal flask app

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask, jsonify
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from collections import Counter
-import nltk
-
-nltk.download('punkt')
-nltk.download('stopwords')
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def word_frequency():
-    text = "Hello world! This is a test. Hello again."
-    stop_words = set(stopwords.words('english'))
-
-    word_tokens = word_tokenize(text)
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
-
-    word_freq = Counter(filtered_sentence)
-    
-    return jsonify(dict(word_freq))
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(port=80, debug=False)
+    app.run(debug=True)
